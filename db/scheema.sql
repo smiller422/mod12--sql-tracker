93fcf608eea3f401e69e4a5663bed2e70b516780
Sarah~ <s.k.miller422@gmail.com>
2023-04-11 19:04:06 -0400

    commits

---
+++ db/scheema.sql
@@ -0,0 +1,35 @@
+-- scheema should contain the following 3 tbls:
+-- department, role, employee
+
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    title VARCHAR(30) NOT NULL
+    salary DECIMAL(10, 2) NOT NULL
+    department_id INT NOT NULL
+     FOREIGN KEY (department_id)
+     REFERENCES department(id)
+);
+
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL
+  last_name VARCHAR(30) NOT NULL 
+  role_id INT NOT NULL
+  manager_id INT 
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+);
---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const data = require('./terms.json');
+
+// npm install mysql2 inquirer console.table
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_db'
+  });
+  connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to database');
+  });
+
+
+const PORT = 3001;
+
+const app = express();
+
+
+inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ])
+  .then(answers => {
+    switch (answers.action) {
+      case 'View all departments':
+        // Perform action to view all departments
+        break;
+      case 'View all roles':
+        // Perform action to view all roles
+        break;
+      case 'View all employees':
+        // Perform action to view all employees
+        break;
+      case 'Add a department':
+        // Perform action to add a department
+        break;
+      case 'Add a role':
+        // Perform action to add a role
+        break;
+      case 'Add an employee':
+        // Perform action to add an employee
+        break;
+      case 'Update an employee role':
+        // Perform action to update an employee role
+        break;
+      case 'Exit':
+        // Exit the application
+        process.exit();
+        break;
+    }
+  });
+
+  connection.query('SELECT * FROM departments', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+  });
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the name of the department:'
+    }
+  ])
+  .then(answers => {
+    connection.query('INSERT INTO departments SET ?', { name: answers.name }, (err, results) => {
+      if (err) throw err;
+      console.log('Department added successfully');
+    });
+  });
+
+  connection.query('SELECT * FROM employees', (err, results) => {
+    if (err) throw err;
+    const choices = results.map(employee => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }));
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'employeeId'
+      }
+    ]);
+  });
